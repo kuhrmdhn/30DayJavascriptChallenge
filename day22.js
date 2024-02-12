bc244be8705842db56dfba11c30fce1f449750d8
kuh.rmdhn <kukuhardi669@gmail.com>
2024-02-12 20:05:15 +0700

    day 22 => Mumbling

---
+++ day22.js
@@ -0,0 +1,17 @@
+// This time no story, no theory. The examples below show you how to write function accum:
+// Examples:
+// accum("abcd") -> "A-Bb-Ccc-Dddd"
+// accum("RqaEzty") -> "R-Qq-Aaa-Eeee-Zzzzz-Tttttt-Yyyyyyy"
+// accum("cwAt") -> "C-Ww-Aaa-Tttt"
+// The parameter of accum is a string which includes only letters from a..z and A..Z.
+
+// Source => https://www.codewars.com/kata/5667e8f4e3f572a8f2000039
+
+const accum = s => s.split("").map((e,i) => e.toUpperCase() + e.repeat(i).toLowerCase()).join("-")
+
+// TEST
+console.log(accum("ZpglnRxqenU")); // must return 'Z-Pp-Ggg-Llll-Nnnnn-Rrrrrr-Xxxxxxx-Qqqqqqqq-Eeeeeeeee-Nnnnnnnnnn-Uuuuuuuuuuu'
+console.log(accum("NyffsGeyylB")); // must return 'N-Yy-Fff-Ffff-Sssss-Gggggg-Eeeeeee-Yyyyyyyy-Yyyyyyyyy-Llllllllll-Bbbbbbbbbbb'
+console.log(accum("MjtkuBovqrU")); // must return 'M-Jj-Ttt-Kkkk-Uuuuu-Bbbbbb-Ooooooo-Vvvvvvvv-Qqqqqqqqq-Rrrrrrrrrr-Uuuuuuuuuuu'
+console.log(accum("EvidjUnokmM")); // must return 'E-Vv-Iii-Dddd-Jjjjj-Uuuuuu-Nnnnnnn-Oooooooo-Kkkkkkkkk-Mmmmmmmmmm-Mmmmmmmmmmm'
+console.log(accum("HbideVbxncC")); // must return 'H-Bb-Iii-Dddd-Eeeee-Vvvvvv-Bbbbbbb-Xxxxxxxx-Nnnnnnnnn-Cccccccccc-Ccccccccccc'
